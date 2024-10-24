64928e1eebcdca89209b91fa44fa8f9ed7b3e0f8
Onred <39244785+Onred@users.noreply.github.com>
2024-10-24 16:58:54 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './components/Counter'
+import Greeting from './components/Greeting'
+
+function App() {
+  
+  return (
+    <>
+    <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [hideNumber, toggleHideNumber] = useState(false)
+
+  return (
+    <>
+    <h1 className={hideNumber ? 'hidden' : ''}>{count}</h1>
+    
+    <button type="button" onClick={() => setCount(count + 1)}>
+      +1
+    </button>
+
+    <button type="button" onClick={() => setCount(count + 5)}>
+      +5
+    </button>
+
+    <button type="button" onClick={() => setCount(0)}>
+      Reset
+    </button>
+
+    <button type="button" onClick={() => toggleHideNumber(!hideNumber)}>
+      {hideNumber ? 'Unhide' : 'Hide'}
+    </button>
+    </>
+  )
+}
